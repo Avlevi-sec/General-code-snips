fdc81a62eeeeab3d42c056d4b90956af0fcea202
AL <27575408+Avlevi-sec@users.noreply.github.com>
2023-08-28 12:10:15 +0300

    Create keeperextractor.py

---
+++ keeperextractor.py
@@ -0,0 +1,18 @@
+from keepercommander import api
+from keepercommander.commands.enterprise import UserReportCommand, SecurityAuditReportCommand
+from keepercommander.params import KeeperParams
+
+my_params = KeeperParams()
+my_params.user = "avihay.levi@outseer.com"
+my_params.password = "xbi_:sgqr.hM8HBa"
+
+api.login(my_params)
+api.sync_down(my_params)
+
+#searches password based on name and returns a list of matching records
+# result = api.search_records(my_params,"orca")
+# print(vars(result[0]))
+
+#searches password based on ID and thats it
+# record = api.get_record(my_params,"uuokWQH1slUXKvyZuTLhZg")
+# print(vars(record))
